2bad28cc151f44335c4c7d8eae9d63d167fddc6c
luliqwerty <3535768174@qq.com>
2025-10-18 22:42:54 +0800

    solved lowestCommonAncestor

---
+++ data_structure/binary_tree/LCLCR194ErChaShuDeZuiJinGongGongZuXianLcof.java
@@ -0,0 +1,25 @@
+//English description is not available for the problem. Please switch to 
+//Chinese. 
+//
+// 👍 612 👎 0
+
+
+package data_structure.binary_tree;
+
+public class LCLCR194ErChaShuDeZuiJinGongGongZuXianLcof {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
