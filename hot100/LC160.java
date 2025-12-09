cf724100920f3a66cea3f78526119828bd3d6241
huangdongqiao <huangdongqiao@kuaishou.com>
2025-12-09 19:19:26 +0800

    solved MergeKSortedLists of hot100

--- hot100/LC160.java
+++ hot100/LC160.java
@@ -1,14 +1,6 @@
 package hot100;
 
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-
 public class LC160 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
